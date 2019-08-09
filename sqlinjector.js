6e7b2547c2aeb636349d7b8e39ef4087338d90c4
Roberto Manfreda <robertomanfreda1994@gmail.com>
2019-08-10 00:49:40 +0200

    refactor

--- sqlinjector.js
+++ sqlinjector.js
@@ -1,8 +1,12 @@
 //alert("ADDON is executing");
 document.body.style.border = "5px solid blue";
 
+var br = document.createElement("br");
+var w = screen.width;
+var h = screen.height;
+
 //Create array of payloads to be added, these should be SQL payloads
-const array = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+const rawPayloadsDictionary = ["", "1hgvsxcwqytcvqwwucvuq1hgvsxcwqytcvqwvcvquwcuqwucvuq1hgvsxcwqytcvqwvcvquwcuqwucvuq1hgvsxcwqytcvqwvcvquwcuqwucvuq1hgvsxcwqytcvqwvcvquwcuqwucvuq1hgvsxcwqytcvqwvcvquwcuqwucvuq1hgvsxcwqytcvqwvcvquwcuqwucvuq", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 function main() {
     const inputs = document.getElementsByTagName("INPUT");
@@ -20,19 +24,17 @@ function main() {
  * @param element the element where inject the SQL payload
  */
 function createAndAppendPayoladsBox(element) {
-    let payloads_span = document.getElementById("payloads_span");
-    if (null != payloads_span) payloads_span.remove();
+    let container = document.getElementById("container_1");
+    if (null != container) payloadsSpan.remove();
 
     // Create select list
     let payloadsList = document.createElement("select");
-    payloadsList.id = "payloads_select";
 
     // Create and append options
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < rawPayloadsDictionary.length; i++) {
         let payload = document.createElement("option");
-        payload.value = array[i];
-        payload.text = array[i];
-        payload.id = "opt_" + i;
+        payload.value = rawPayloadsDictionary[i];
+        payload.text = rawPayloadsDictionary[i];
         payload.onclick = function () {
             element.value = this.text;
         };
@@ -41,29 +43,48 @@ function createAndAppendPayoladsBox(element) {
         payloadsList.appendChild(payload);
     }
 
+    let textInject = document.createElement("span");
+    textInject.innerHTML = "Inject payloads";
+    textInject.style = "color: white; " + centerElementInADiv(50, 0);
 
-    // Append select list
-    payloads_span = document.createElement("span");
-    payloads_span.id = "payloads_span";
-    payloads_span.innerHTML = "Inject payload";
-    document.body.appendChild(payloads_span);
 
-    payloads_span.style = getStyle();
-
-    payloads_span.appendChild(payloadsList);
-
-    let spanClose = document.createElement("button");
-    spanClose.innerHTML = "X";
-    spanClose.style = "color: red";
-    spanClose.onclick = function () {
-        payloads_span.remove();
+    let buttonClose = document.createElement("button");
+    buttonClose.innerHTML = "X";
+    buttonClose.style = "color: red; float: right;";
+    buttonClose.onclick = function () {
+        container.remove();
     };
 
-    payloads_span.appendChild(spanClose);
+    container = document.createElement("div");
+    container.style = getStyle();
+    container.id = "container_1";
+    container.appendChild(textInject);
+
+    container.appendChild(buttonClose);
+
+    container.appendChild(br);
+
+    let rawPayloadsText = document.createElement("div");
+    rawPayloadsText.innerHTML = "RAW PAYLOADS";
+    rawPayloadsText.style = centerElementInADiv(50, 0) + "margin-top: 20px;";
+    rawPayloadsText.style.color = "red";
+    container.appendChild(rawPayloadsText);
+
+
+    payloadsList.style = "width: inherit";
+    container.appendChild(payloadsList);
+
+    document.body.appendChild(container);
+
+
+}
+
+function centerElementInADiv(horizontalPercentage, verticalPercentage) {
+    return "position: relative; float: left; left: 50%; transform: translate(-" + horizontalPercentage + "%, " + verticalPercentage + "%);"
 }
 
 function getStyle() {
-    return "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);";
+    return "padding: 30px; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background-color: rgba(0, 0, 0, 0.8); width: -moz-available;";
 }
 
 //__________
